2580311212f202075d43f4719314be1444b784ae
NOOB_3301 <sepic466@gmail.com>
2024-08-20 00:39:08 +0530

    shit

--- src/components/manga_comps/MangaHome2.jsx
+++ src/components/manga_comps/MangaHome2.jsx
@@ -1,34 +1,11 @@
-import React from 'react';
-import axios from 'axios';
+import React from 'react'
 
 function MangaHome2() {
-  const title = 'Demon Slayer';
-
-  async function fetch_manga() {
-    try {
-      const response = await axios({
-        method: 'GET',
-        url: '/api/manga',
-        params: {
-          title: title,
-        },
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching manga:', error);
-    }
-  }
-
   return (
     <div>
-      this is 2
-      <button onClick={fetch_manga}>Click me</button>
-      <img
-        src="/manga-cover/covers/7e19c079-3a9d-4f61-88e7-b9c72eb69705/7c928a0c-4ff9-464e-b16f-95c470951914.jpg"
-        alt="lwra img"
-      />
+      test2
     </div>
-  );
+  )
 }
 
-export default MangaHome2;
+export default MangaHome2
