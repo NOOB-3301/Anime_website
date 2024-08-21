9f1a8d4ab48d384fdbb3017e682f485e44ae77fe
NOOB_3301 <sepic466@gmail.com>
2024-08-22 02:47:35 +0530

    ....

--- src/components/manga_comps/Mangadex22.jsx
+++ src/components/manga_comps/Mangadex22.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import axios from 'axios'
+
+const info_url = "/aviv2/manga/info/"
+
+
 
 function Mangadex22() {
+
+  async function fetch_info(url,query) {
+    let full = `${url}${query}`
+    const resp = await axios.get(full)
+    console.log(resp.data)
+  }
   return (
     <div>
       this is manga dex
+      <button onClick={()=>fetch_info(info_url,'naruto')} >clickkk</button>
     </div>
   )
 }
