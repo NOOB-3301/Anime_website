d5155a0141abb95ef806bfc368b79806b9b7c891
NOOB_3301 <sepic466@gmail.com>
2024-08-21 21:43:22 +0530

    ....

--- src/components/manga_comps/Managadex.jsx
+++ src/components/manga_comps/Managadex.jsx
@@ -21,6 +21,7 @@ function Mangadex() {
       <h1>This is MangaDex</h1>
       <img id="manga-image" alt="Manga Cover" />
       <button onClick={fetchImage}>Fetch Jujutsu Kaisen Image</button>
+      <img src="https://uploads.mangadex.org/covers/8f3e1818-a015-491d-bd81-3addc4d7d56a/26dd2770-d383-42e9-a42b-32765a4d99c8.png" alt="image" />
     </div>
   );
 }
