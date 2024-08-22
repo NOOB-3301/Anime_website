3867b7ab22829a15c508a14147c19e8fdc9afa0d
NOOB_3301 <sepic466@gmail.com>
2024-08-23 03:32:15 +0530

    ....

--- src/components/manga_comps/MangaDesc.jsx
+++ src/components/manga_comps/MangaDesc.jsx
@@ -8,29 +8,22 @@ const img_url = '/avi2/manga/image/';
 function MangaDesc() {
   let { manga_id } = useParams();
   const [fileName, setFileName] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
   async function fetch_cover(url, query) {
     let full = `${url}${query}`;
-    const resp = await axios.get(full);
-    console.log(resp.data.data);
+    try {
+      const resp = await axios.get(full);
+      console.log(resp.data.data);
 
-    if (resp.data.data.length > 0) {
-      const fetchedFileName = resp.data.data[0].attributes.fileName;
-      setFileName(fetchedFileName);
+      if (resp.data.data.length > 0) {
+        const fetchedFileName = resp.data.data[0].attributes.fileName;
+        setFileName(fetchedFileName);
+      }
+    } catch (error) {
+      console.error("Failed to fetch cover data:", error);
     }
   }
-
-  // async function fetch_cover_image(id, file) {
-  //   if (file) {
-  //     let full = `${img_url}${id}/${file}`;
-  //     try {
-  //       const resp = await axios.get(full);
-  //       console.log(resp.data);
-  //     } catch (error) {
-  //       console.error("Failed to fetch cover image:", error);
-  //     }
-  //   }
-  // }
 
   async function fetch_cover_image(id, file) {
     if (file) {
@@ -48,6 +41,7 @@ function MangaDesc() {
     }
 }
 
+
   useEffect(() => {
     fetch_cover(cover_url, manga_id);
   }, [manga_id]);
@@ -58,8 +52,9 @@ function MangaDesc() {
 
   return (
     <div>
-      This is manga description {manga_id}
-      and file name {fileName}
+      <p>This is manga description {manga_id}</p>
+      <p>File name: {fileName}</p>
+      {imageUrl && <img src={imageUrl} alt="Manga Cover" />}
     </div>
   );
 }
