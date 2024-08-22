5b70429288bc9e9183789b48aa26c3b610075960
NOOB_3301 <sepic466@gmail.com>
2024-08-23 03:17:58 +0530

    chat gpttttttttt

--- src/components/manga_comps/MangaDesc.jsx
+++ src/components/manga_comps/MangaDesc.jsx
@@ -1,52 +1,51 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react' 
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
+const cover_url = "/aviv2/manga/cover/";
+const img_url = '/avi2/manga/image/';
 
-const cover_url = "/aviv2/manga/cover/"
-
-const img_url = '/avi2/manga/image/'
 function MangaDesc() {
+  let { manga_id } = useParams();
+  const [fileName, setFileName] = useState('');
 
-    let {manga_id} = useParams()
-    const [fileName, setFileName] = useState('')
+  async function fetch_cover(url, query) {
+    let full = `${url}${query}`;
+    const resp = await axios.get(full);
+    console.log(resp.data.data);
 
-    async function fetch_cover_image(id,file) {
-      let full = `/avi2/manga/image//${id}/${file}`
-      const resp = await axios.get(full)
-      console.log(resp)
+    if (resp.data.data.length > 0) {
+      const fetchedFileName = resp.data.data[0].attributes.fileName;
+      setFileName(fetchedFileName);
     }
+  }
 
-    async function fetch_cover(url,query) {
-      let full = `${url}${query}`
-      const resp = await axios.get(full)
-      console.log(resp.data.data)
-      setFileName(resp.data.data[0].attributes.fileName)
-      console.log(resp.data.data[0].attributes.fileName)
-      fetch_cover_image(manga_id, fileName)
+  async function fetch_cover_image(id, file) {
+    if (file) {
+      let full = `${img_url}${id}/${file}`;
+      try {
+        const resp = await axios.get(full);
+        console.log(resp);
+      } catch (error) {
+        console.error("Failed to fetch cover image:", error);
+      }
     }
+  }
 
+  useEffect(() => {
+    fetch_cover(cover_url, manga_id);
+  }, [manga_id]);
 
-
-    useEffect(() => {
-      fetch_cover(cover_url,manga_id)
-      // fetch_cover_image(manga_id, fileName)
-    }, [])
-
-    // useEffect(() => {
-    //   fetch_cover_image(manga_id, fileName)
-    // }, [])
-    
-    
-
+  useEffect(() => {
+    fetch_cover_image(manga_id, fileName);
+  }, [fileName, manga_id]);
 
   return (
     <div>
-      thhis is manga desc {manga_id}
+      This is manga description {manga_id}
       and file name {fileName}
     </div>
-  )
+  );
 }
 
-export default MangaDesc
+export default MangaDesc;
